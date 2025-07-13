2ab84eb353f8cd8273e2da68d6f7ce5414df92b5
Sufyaan Ahmed <ahmedsemailis@gmail.com>
2025-07-13 21:06:43 +0530

    added the fetched stats to the dashboard

--- pages/api/trade.js
+++ pages/api/trade.js
@@ -28,7 +28,6 @@ export default async function handler(req, res) {
         maxPoolSize: 10,
         serverSelectionTimeoutMS: 5000,
         socketTimeoutMS: 45000,
-        bufferMaxEntries: 0,
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
